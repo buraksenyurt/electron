69c8d28b6c10bcf9dde07940dfafd91f41206d2b
buraksenyurt <burakselimsenyurt@gmail.com>
2018-11-10 23:20:53 +0300

    using bootstrap and jquery in electron

---
+++ Lesson02/main.js
@@ -0,0 +1,16 @@
+const { app, BrowserWindow } = require('electron')
+const url = require('url')
+const path = require('path')
+
+let win
+
+function createWindow() {
+    win = new BrowserWindow({ width: 640, height: 360 })
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
+}
+
+app.on('ready', createWindow)
